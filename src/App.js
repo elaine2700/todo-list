7e12c6cfc925c5c8c519dd0497ab7df99bb7cfd9
elaine2700 <easerrano.e@gmail.com>
2023-03-23 20:28:28 -0700

    Functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import React, { useState } from "react";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  display:inline-block;
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: white;
+  height: 30px;
+  width: 50px;
+  border-radius: 2px;
+  cursor: pointer;
+`;
+
+const Text = styled.input`
+ border: 2px solid #000;
+`;
+
+const TaskCount = styled.span`
+  margin: 10px;
+`;
+const Tasks = styled.div`
+`;
+
+const LIST = styled.li`
+    listStyle:"none";
+    text-decoration: "line-through";
+`;
+  
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [completedTaskCount, setCompletedTaskCount] = useState(0);
+
+  const handleClick = () => {
+    const id = todoList.length + 1;
+    setTodoList((prev) => [
+      ...prev,
+      {
+        id: id,
+        task: input,
+        complete: false,
+      },
+    ]);
+    setInput("");
+  };
+
+  const Clear = ()=> {
+    setTodoList(() => []);
+    setCompletedTaskCount(0);
+  };
+
+  const handleComplete = (id) => {
+    let list = todoList.map((task) => {
+      let item = {};
+      if (task.id == id) {
+        if (!task.complete){
+            //Task is pending, modifying it to complete and increment the count
+            setCompletedTaskCount(completedTaskCount + 1);
+        } 
+        else {
+            //Task is complete, modifying it back to pending, decrement Complete count
+            setCompletedTaskCount(completedTaskCount - 1);
+        }
+item = { ...task, complete: !task.complete };
+      } else item = { ...task };
+return item;
+    });
+    setTodoList(list);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div>
+          <h2>Todo List</h2>
+          <Text value={input} onInput={(e) =>setInput(e.target.value)} />
+          <Button onClick={() => handleClick()}>Add</Button>
+        <Tasks>
+          <TaskCount>
+            <b>Pending Tasks</b> {todoList.length - completedTaskCount}
+          </TaskCount>
+          <TaskCount>
+            <b>Completed Tasks</b> {completedTaskCount}
+          </TaskCount>
+        </Tasks>
+        <div>
+          <ul>
+            {todoList.map((todo) => {
+              return (
+                <LIST
+                  complete = {todo.complete}
+                  id={todo.id}
+                  onClick={() => handleComplete(todo.id)}
+                  style={{
+                    listStyle: "none",
+                    textDecoration: todo.complete && "line-through",
+                  }}
+                >
+                  {todo.task}
+                </LIST>
+              );
+            })}
+          </ul>
+        </div>
+        <Button onClick={() => Clear()}>Clear</Button>
+      </div>
+    </Container>
   );
 }
 
